469ac0ef2324cb16486068f3c0128105ff3e703a
Mathias Ertl <mati@fsinf.at>
2010-12-24 13:50:10 +0000

    add setup.py and license disclaimers

--- setup.py
+++ setup.py
@@ -13,26 +13,16 @@
 #    You should have received a copy of the GNU General Public License
 #    along with RestAuthClient.py.  If not, see <http://www.gnu.org/licenses/>.
 
-import handlers
 
-CONTENT_HANDLERS = { 'application/json': handlers.json, 
-	'application/xml': handlers.xml,
-	'application/x-www-form-urlencoded': handlers.form }
+from distutils.core import setup
 
-def marshal( content_type, obj ):
-	handler = CONTENT_HANDLERS[content_type]()
-	return handler.marshal( obj )
-
-def unmarshal( content_type, raw_data, typ ):
-	handler = CONTENT_HANDLERS[content_type]()
-	return handler.unmarshal( raw_data, typ )
-
-def unmarshal_dict( content_type, raw_data, keys ):
-	data = unmarshal( content_type, raw_data, dict )
-	if sorted( keys ) != sorted( data.keys() ):
-		raise MarshalError( "Did not find expected keys in string" ) 
-	if len( keys ) == 1:
-		return data[keys[0]]
-	else:
-		return [ data[key] for key in keys ]
-
+setup(
+	name='restauth-common',
+	version='1.0',
+	description='RestAuth shared library',
+	author='Mathias Ertl',
+	author_email='mati@fsinf.at',
+	url='http://fs.fsinf.at/wiki/RestAuth',
+	package_dir = {'': 'python'},
+	packages=['RestAuthCommon']
+)
