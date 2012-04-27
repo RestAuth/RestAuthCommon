c507e57a3c0a1b7aaee51966ed3a87d2f296d134
Mathias Ertl <mati@fsinf.at>
2012-04-28 01:18:39 +0200

    adhere to coding standards

--- python/RestAuthCommon/__init__.py
+++ python/RestAuthCommon/__init__.py
@@ -18,102 +18,102 @@ A collection of functions used in both server and client reference implementatio
 .. moduleauthor:: Mathias Ertl <mati@restauth.net>
 """
 try:
-	from RestAuthCommon.handlers import CONTENT_HANDLERS
+    from RestAuthCommon.handlers import CONTENT_HANDLERS
 except ImportError:
-	# python2.5 and earlier
-	import handlers.CONTENT_HANDLERS as CONTENT_HANDLERS
+    # python2.5 and earlier
+    import handlers.CONTENT_HANDLERS as CONTENT_HANDLERS
 
-def marshal( content_type, obj ):
-	"""
-	Marshal the object *obj* into a string of the MIME type *content_type*.
-	
-	This method is just intended as a shortcut for :py:meth:`.content_handler.unmarshal`. If you
-	intend to use a handler multiple times, it is better to instantiate a specific handler
-	directly to save dictionary lookups and object instantiations.
-	
-	.. WARNING:: This method will be deprecated soon.
+def marshal(content_type, obj):
+    """
+    Marshal the object *obj* into a string of the MIME type *content_type*.
+    
+    This method is just intended as a shortcut for :py:meth:`.content_handler.unmarshal`. If you
+    intend to use a handler multiple times, it is better to instantiate a specific handler
+    directly to save dictionary lookups and object instantiations.
+    
+    .. WARNING:: This method will be deprecated soon.
 
-	:param content_type: The format that the object should be marshalled into. This has to be
-		one of the keys defined in :py:obj:`.CONTENT_HANDLERS`.
-	:type  content_type: str
-	:param obj: The object to marshal.
+    :param content_type: The format that the object should be marshalled into. This has to be
+        one of the keys defined in :py:obj:`.CONTENT_HANDLERS`.
+    :type  content_type: str
+    :param obj: The object to marshal.
 
-	:return: The string representation of the object.
-	:rtype:  str
+    :return: The string representation of the object.
+    :rtype:  str
 
-	:raise error.MarshalError: When the handler could not marshal the object.
-	:see also: :py:meth:`.content_handler.marshal`
-	"""
-	handler = CONTENT_HANDLERS[content_type]()
-	return handler.marshal( obj )
+    :raise error.MarshalError: When the handler could not marshal the object.
+    :see also: :py:meth:`.content_handler.marshal`
+    """
+    handler = CONTENT_HANDLERS[content_type]()
+    return handler.marshal(obj)
 
-def unmarshal( content_type, raw_data, typ ):
-	"""
-	Unmarshal the string *raw_data* into an object of type *typ*. The string is assumed to be of
-	the MIME type *content_type*.
+def unmarshal(content_type, raw_data, typ):
+    """
+    Unmarshal the string *raw_data* into an object of type *typ*. The string is assumed to be of
+    the MIME type *content_type*.
 
-	This method is just intended as a shortcut for :py:meth:`.content_handler.unmarshal`. If you
-	intend to use a handler multiple times, it is better to instantiate a specific handler
-	directly to save dictionary lookups and object instantiations.
-	
-	.. WARNING:: This method will be deprecated soon.
-	
-	:param content_type: The format that the object should be marshalled into. This has to be
-		one of the keys defined in :py:obj:`.CONTENT_HANDLERS`.
-	:type  content_type: str
-	:param raw_data: The raw string that should be unmarshalled.
-	:type  raw_data: str
-	:param typ: The type the unmarshaled object should be of.
-	
-	:rtype: typ
-	:return: The unmarshalled data. The object has the type specified by the I{typ} parameter.
-	
-	:raise error.UnmarshalError: When the handler was unable unmarshal the object.
+    This method is just intended as a shortcut for :py:meth:`.content_handler.unmarshal`. If you
+    intend to use a handler multiple times, it is better to instantiate a specific handler
+    directly to save dictionary lookups and object instantiations.
+    
+    .. WARNING:: This method will be deprecated soon.
+    
+    :param content_type: The format that the object should be marshalled into. This has to be
+        one of the keys defined in :py:obj:`.CONTENT_HANDLERS`.
+    :type  content_type: str
+    :param raw_data: The raw string that should be unmarshalled.
+    :type  raw_data: str
+    :param typ: The type the unmarshaled object should be of.
+    
+    :rtype: typ
+    :return: The unmarshalled data. The object has the type specified by the I{typ} parameter.
+    
+    :raise error.UnmarshalError: When the handler was unable unmarshal the object.
 
-	:see also: :py:meth:`.content_handler.unmarshal`
-	"""
-	handler = CONTENT_HANDLERS[content_type]()
-	return handler.unmarshal( raw_data, typ )
+    :see also: :py:meth:`.content_handler.unmarshal`
+    """
+    handler = CONTENT_HANDLERS[content_type]()
+    return handler.unmarshal(raw_data, typ)
 
-def resource_validator( name ):
-	"""
-	.. WARNING:: This code will be moved to its own submodule soon.
-	
-	Check the *name* of a resource for some really bad characters that shouldn't be used
-	anywhere in RestAuth. 
+def resource_validator(name):
+    """
+    .. WARNING:: This code will be moved to its own submodule soon.
+    
+    Check the *name* of a resource for some really bad characters that shouldn't be used
+    anywhere in RestAuth. 
 
-	This filters names containing a slash ("/") or colon (":") and those starting with '.'. It
-	also filters control characters etc., including those from unicode.
-	
-	:param str name: The name to validate
-	:returns: False if the name contains any invalid characters, True otherwise.
-	:rtype: bool
-	"""
-	if '/' in name or ':' in name or '\\' in name or name.startswith( '.' ):
-		return False
+    This filters names containing a slash ("/") or colon (":") and those starting with '.'. It
+    also filters control characters etc., including those from unicode.
+    
+    :param str name: The name to validate
+    :returns: False if the name contains any invalid characters, True otherwise.
+    :rtype: bool
+    """
+    if '/' in name or ':' in name or '\\' in name or name.startswith('.'):
+        return False
 
-	# filter various dangerous characters
-	import stringprep
-	for enc_char in name:
-		if enc_char.__class__ == str:
-			enc_char = enc_char.decode( 'utf-8' )
+    # filter various dangerous characters
+    import stringprep
+    for enc_char in name:
+        if enc_char.__class__ == str:
+            enc_char = enc_char.decode('utf-8')
 
-		if stringprep.in_table_c21_c22( enc_char ):
-			# control characters
-			return False
-		if stringprep.in_table_c3( enc_char ):
-			return False
-		if stringprep.in_table_c4( enc_char ):
-			return False
-		if stringprep.in_table_c5( enc_char ):
-			return False
-		if stringprep.in_table_c6( enc_char ):
-			return False
-		if stringprep.in_table_c7( enc_char ):
-			return False
-		if stringprep.in_table_c8( enc_char ):
-			return False
-		if stringprep.in_table_c9( enc_char ):
-			return False
+        if stringprep.in_table_c21_c22(enc_char):
+            # control characters
+            return False
+        if stringprep.in_table_c3(enc_char):
+            return False
+        if stringprep.in_table_c4(enc_char):
+            return False
+        if stringprep.in_table_c5(enc_char):
+            return False
+        if stringprep.in_table_c6(enc_char):
+            return False
+        if stringprep.in_table_c7(enc_char):
+            return False
+        if stringprep.in_table_c8(enc_char):
+            return False
+        if stringprep.in_table_c9(enc_char):
+            return False
 
-	return True
+    return True
--- python/RestAuthCommon/handlers.py
+++ python/RestAuthCommon/handlers.py
@@ -21,252 +21,252 @@ Classes and methods related to content handling.
 import sys
 
 try:
-	from RestAuthCommon import error
+    from RestAuthCommon import error
 except ImportError:
-	# python2.5 and earlier
-	import error
+    # python2.5 and earlier
+    import error
 
-class content_handler( object ):
-	"""
-	This class is a common base class for all content handlers. If you
-	want to implement your own content handler, you must subclass this
-	class and implement all marshal_* and unmarshal_* methods.
-	
-	**Never use this class directly.** It does not marshal or unmarshal any content itself.
-	"""
+class content_handler(object):
+    """
+    This class is a common base class for all content handlers. If you
+    want to implement your own content handler, you must subclass this
+    class and implement all marshal_* and unmarshal_* methods.
+    
+    **Never use this class directly.** It does not marshal or unmarshal any content itself.
+    """
 
-	mime = None
-	"""Override this with the MIME type handled by your handler."""
-	
-	def marshal( self, obj ):
-		"""
-		Shortcut for marshalling just any object. 
-		
-		**Note:** If you know the type of **obj** in advance, you should
-		use the marshal_* methods directly for improved speed.
+    mime = None
+    """Override this with the MIME type handled by your handler."""
+    
+    def marshal(self, obj):
+        """
+        Shortcut for marshalling just any object. 
+        
+        **Note:** If you know the type of **obj** in advance, you should
+        use the marshal_* methods directly for improved speed.
 
-		:param obj: The object to marshall.
-		:return: The marshalled representation of the object.
-		:rtype: str
-		:raise error.MarshalError: If marshalling goes wrong in any way.
-		"""
-		func_name = 'marshal_%s'%(obj.__class__.__name__)
-		try:
-			func = getattr( self, func_name )
-			return func( obj )
-		except error.MarshalError as e:
-			raise e
-		except Exception as e:
-			raise error.MarshalError( e )
+        :param obj: The object to marshall.
+        :return: The marshalled representation of the object.
+        :rtype: str
+        :raise error.MarshalError: If marshalling goes wrong in any way.
+        """
+        func_name = 'marshal_%s'%(obj.__class__.__name__)
+        try:
+            func = getattr(self, func_name)
+            return func(obj)
+        except error.MarshalError as e:
+            raise e
+        except Exception as e:
+            raise error.MarshalError(e)
 
-	def unmarshal( self, raw_data, typ ):
-		"""
-		Shortcut for unmarshalling a string to an object of type *typ*.
-		
-		**Note:** You may want to use the unmarshal_* methods directly
-		for improved speed.
+    def unmarshal(self, raw_data, typ):
+        """
+        Shortcut for unmarshalling a string to an object of type *typ*.
+        
+        **Note:** You may want to use the unmarshal_* methods directly
+        for improved speed.
 
-		:param raw_data: The string to unmarshall.
-		:type  raw_data: str
-		:param typ: The typ of the unmarshalled object.
-		:type  typ: type
-		:return: The unmarshalled object.
-		:rtype: typ
-		:raise error.UnmarshalError: If unmarshalling goes wrong in any way.
-		"""
-		try:
-			func = getattr( self, 'unmarshal_%s'%(typ.__name__) )
-			val = func( raw_data )
-		except error.UnmarshalError as e:
-			raise e
-		except Exception as e:
-			raise error.UnmarshalError( e )
-		
-		if val.__class__ != typ:
-			raise error.UnmarshalError( "Request body contained %s instead of %s"
-			                        %(val.__class__, typ) )
-		return val
+        :param raw_data: The string to unmarshall.
+        :type  raw_data: str
+        :param typ: The typ of the unmarshalled object.
+        :type  typ: type
+        :return: The unmarshalled object.
+        :rtype: typ
+        :raise error.UnmarshalError: If unmarshalling goes wrong in any way.
+        """
+        try:
+            func = getattr(self, 'unmarshal_%s'%(typ.__name__))
+            val = func(raw_data)
+        except error.UnmarshalError as e:
+            raise e
+        except Exception as e:
+            raise error.UnmarshalError(e)
+        
+        if val.__class__ != typ:
+            raise error.UnmarshalError("Request body contained %s instead of %s"
+                                    %(val.__class__, typ))
+        return val
 
-	def unmarshal_str( self, data ):
-		"""
-		Unmarshal a string.
-		"""
-		pass
-	
-	def unmarshal_dict( self, body ):
-		"""
-		Unmarshal a dictionary.
-		"""
-		pass
-	def unmarshal_list( self, body ):
-		"""
-		Unmarshal a list.
-		"""
-		pass
-	
-	def unmarshal_bool( self, body ):
-		"""
-		Unmarshal a boolean.
-		"""
-		pass
+    def unmarshal_str(self, data):
+        """
+        Unmarshal a string.
+        """
+        pass
+    
+    def unmarshal_dict(self, body):
+        """
+        Unmarshal a dictionary.
+        """
+        pass
+    def unmarshal_list(self, body):
+        """
+        Unmarshal a list.
+        """
+        pass
+    
+    def unmarshal_bool(self, body):
+        """
+        Unmarshal a boolean.
+        """
+        pass
 
-	def marshal_str( self, obj ):
-		"""
-		Marshal a string.
-		"""
-		pass
-	
-	def marshal_bool( self, obj ):
-		"""
-		Marshal a boolean.
-		"""
-		pass
-	
-	def marshal_list( self, obj ):
-		"""
-		Marshal a list.
-		"""
-		pass
-	def marshal_dict( self, obj ):
-		"""
-		Marshal a dictionary.
-		"""
-		pass
-	
-	def marshal_unicode( self, obj ):
-		return self.marshal_str( obj.encode( 'utf-8' ) )
+    def marshal_str(self, obj):
+        """
+        Marshal a string.
+        """
+        pass
+    
+    def marshal_bool(self, obj):
+        """
+        Marshal a boolean.
+        """
+        pass
+    
+    def marshal_list(self, obj):
+        """
+        Marshal a list.
+        """
+        pass
+    def marshal_dict(self, obj):
+        """
+        Marshal a dictionary.
+        """
+        pass
+    
+    def marshal_unicode(self, obj):
+        return self.marshal_str(obj.encode('utf-8'))
 
-class json( content_handler ):
-	"""
-	Concrete implementation of a :py:class:`content_handler` that uses JSON. This is the default
-	content handler in both server and client library.
-	"""
-	
-	mime = 'application/json'
-	"""The mime-type used by this content handler is 'application/json'."""
+class json(content_handler):
+    """
+    Concrete implementation of a :py:class:`content_handler` that uses JSON. This is the default
+    content handler in both server and client library.
+    """
+    
+    mime = 'application/json'
+    """The mime-type used by this content handler is 'application/json'."""
 
-	def __init__( self ):
-		import json
-		self.json = json
+    def __init__(self):
+        import json
+        self.json = json
 
-	def unmarshal_str( self, body ):
-		try:
-			pure = self.json.loads( body )
-			if pure.__class__ != list or len( pure ) != 1:
-				raise error.UnmarshalError( "Could not parse body as string" )
-		
-			return pure[0]
-		except ValueError:
-			raise error.UnmarshalError( e )
+    def unmarshal_str(self, body):
+        try:
+            pure = self.json.loads(body)
+            if pure.__class__ != list or len(pure) != 1:
+                raise error.UnmarshalError("Could not parse body as string")
+        
+            return pure[0]
+        except ValueError:
+            raise error.UnmarshalError(e)
 
-	def unmarshal_dict( self, body ):
-		try:
-			return self.json.loads( body )
-		except ValueError as e:
-			raise error.UnmarshalError( e )
+    def unmarshal_dict(self, body):
+        try:
+            return self.json.loads(body)
+        except ValueError as e:
+            raise error.UnmarshalError(e)
 
-	def unmarshal_list( self, body ):
-		try:
-			return self.json.loads( body )
-		except ValueError as e:
-			raise error.UnmarshalError( e )
+    def unmarshal_list(self, body):
+        try:
+            return self.json.loads(body)
+        except ValueError as e:
+            raise error.UnmarshalError(e)
 
-	def unmarshal_bool( self, body ):
-		try:
-			return self.json.loads( body )
-		except ValueError as e:
-			raise error.UnmarshalError( e )
-	
-	def marshal_str( self, obj ):
-		try:
-			return self.json.dumps( [obj], separators=(',', ':') )
-		except ValueError as e:
-			raise error.MarshalError( e )
+    def unmarshal_bool(self, body):
+        try:
+            return self.json.loads(body)
+        except ValueError as e:
+            raise error.UnmarshalError(e)
+    
+    def marshal_str(self, obj):
+        try:
+            return self.json.dumps([obj], separators=(',', ':'))
+        except ValueError as e:
+            raise error.MarshalError(e)
 
-	def marshal_bool( self, obj ):
-		try:
-			return self.json.dumps( obj, separators=(',', ':') )
-		except ValueError as e:
-			raise error.MarshalError( e )
+    def marshal_bool(self, obj):
+        try:
+            return self.json.dumps(obj, separators=(',', ':'))
+        except ValueError as e:
+            raise error.MarshalError(e)
 
-	def marshal_list( self, obj ):
-		try:
-			return self.json.dumps( obj, separators=(',', ':') )
-		except ValueError as e:
-			raise error.MarshalError( e )
+    def marshal_list(self, obj):
+        try:
+            return self.json.dumps(obj, separators=(',', ':'))
+        except ValueError as e:
+            raise error.MarshalError(e)
 
-	def marshal_dict( self, obj ):
-		try:
-			return self.json.dumps( obj, separators=(',', ':') )
-		except ValueError as e:
-			raise error.MarshalError( e )
-	
-	def marshal_unicode( self, obj ):
-		try:
-			return self.json.dumps( [obj], separators=(',', ':') )
-		except ValueError as e:
-			raise error.MarshalError( e )
+    def marshal_dict(self, obj):
+        try:
+            return self.json.dumps(obj, separators=(',', ':'))
+        except ValueError as e:
+            raise error.MarshalError(e)
+    
+    def marshal_unicode(self, obj):
+        try:
+            return self.json.dumps([obj], separators=(',', ':'))
+        except ValueError as e:
+            raise error.MarshalError(e)
 
-class form( content_handler ):
-	"""
-	Concrete implementation of a :py:class:`content_handler` that uses HTML forms. This content
-	handler should not be used in any real world scenario, as it has many problems with unicode.
-	"""
-	
-	mime = 'application/x-www-form-urlencoded'
-	"""The mime-type used by this content handler is 'application/x-www-form-urlencoded'."""
+class form(content_handler):
+    """
+    Concrete implementation of a :py:class:`content_handler` that uses HTML forms. This content
+    handler should not be used in any real world scenario, as it has many problems with unicode.
+    """
+    
+    mime = 'application/x-www-form-urlencoded'
+    """The mime-type used by this content handler is 'application/x-www-form-urlencoded'."""
 
-	def __init__( self ):
-		try:
-			from urllib.parse import parse_qs, urlencode
-		except ImportError:
-			from urlparse import parse_qs
-			from urllib import urlencode
-		self.parse_qs = parse_qs
-		self.urlencode = urlencode
+    def __init__(self):
+        try:
+            from urllib.parse import parse_qs, urlencode
+        except ImportError:
+            from urlparse import parse_qs
+            from urllib import urlencode
+        self.parse_qs = parse_qs
+        self.urlencode = urlencode
 
-	def unmarshal_dict( self, body ):
-		return_dict = {}
-		for key, value in self.parse_qs( body, True ).iteritems():
-			return_dict[key] = value[0]
-		return return_dict
+    def unmarshal_dict(self, body):
+        return_dict = {}
+        for key, value in self.parse_qs(body, True).iteritems():
+            return_dict[key] = value[0]
+        return return_dict
 
-	def marshal_str( self, obj ):
-		return obj
+    def marshal_str(self, obj):
+        return obj
 
-	def marshal_bool( self, obj ):
-		if obj:
-			return "1"
-		else:
-			return "0"
+    def marshal_bool(self, obj):
+        if obj:
+            return "1"
+        else:
+            return "0"
 
-	def marshal_dict( self, obj ):
-		return self.urlencode( obj )
+    def marshal_dict(self, obj):
+        return self.urlencode(obj)
 
-	def marshal_list( self, obj ):
-		d = dict( [ ('key%s'%i, obj[i]) for i in range(0,len(obj)) ] )
-		return self.marshal_dict( d )
+    def marshal_list(self, obj):
+        d = dict([ ('key%s'%i, obj[i]) for i in range(0,len(obj)) ])
+        return self.marshal_dict(d)
 
-class xml( content_handler ):
-	"""
-	Future location of the XML content handler. This handler is not yet implemented!
-	"""
-	mime = 'application/xml'
-	
+class xml(content_handler):
+    """
+    Future location of the XML content handler. This handler is not yet implemented!
+    """
+    mime = 'application/xml'
+    
 CONTENT_HANDLERS = { 'application/json': json, 
-	'application/xml': xml,
-	'application/x-www-form-urlencoded': form }
+    'application/xml': xml,
+    'application/x-www-form-urlencoded': form }
 """
 Mapping of MIME types to their respective handler implemenation. You can use this dictionary to
 dynamically look up a content handler if you do not know the requested content type in advance.
 
-================================= ========================== =====
+================================= ========================== =========================
 MIME type                         handler                    notes
-================================= ========================== =====
+================================= ========================== =========================
 application/json                  :py:class:`.handlers.json` default
 application/x-www-form-urlencoded :py:class:`.handlers.form` Only use this for testing
-application/xml	                  :py:class:`.handlers.xml`  not yet implemented
-================================= ========================== =====
+application/xml                   :py:class:`.handlers.xml`  not yet implemented
+================================= ========================== =========================
 
 If you want to provide your own implementation of a :py:class:`.content_handler`, you can add it to
 this dictionary with the appropriate MIME type as the key.
