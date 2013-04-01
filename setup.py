896f926eda6f25cbd96bc6f740e68da460589896
Mathias Ertl <mati@fsinf.at>
2013-04-01 16:23:01 +0200

    add python2 identifier for pypi.

--- setup.py
+++ setup.py
@@ -204,6 +204,7 @@ setup(
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP",
